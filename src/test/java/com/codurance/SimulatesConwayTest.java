fc07264dd67ea1fbe0e90508c8e89829236c22a1
tomspencerlondon <thomas.spencer@codurance.com>
2020-09-13 21:07:23 +0100

    Add test class

---
+++ src/test/java/com/codurance/SimulatesConwayTest.java
@@ -0,0 +1,17 @@
+package com.codurance;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class SimulatesConwayTest {
+    @InjectMocks
+    SimulatesConway subject;
+
+    @Test
+    void name() {
+
+    }
+}
