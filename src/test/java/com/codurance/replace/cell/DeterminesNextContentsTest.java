7f7e9d84465bbd1cdba8c94d4589aa82320c5d35
tomspencerlondon <thomas.spencer@codurance.com>
2020-09-14 08:01:27 +0100

    Edit imports

--- src/test/java/com/codurance/replace/cell/DeterminesNextContentsTest.java
+++ src/test/java/com/codurance/replace/cell/DeterminesNextContentsTest.java
@@ -7,14 +7,11 @@ import com.codurance.values.Point;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.Is.isA;
 
 class DeterminesNextContentsTest {
 
